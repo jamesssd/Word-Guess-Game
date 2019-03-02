22115ae7fee0ea570db2d07f5b9e64ad2d227ea4
James Dizon <dizonjerlin@gmail.com>
2019-03-01 23:38:38 -0800

    UpdateFiles

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ How to Play:
 The theme for this game is Washington State Cities. Using the letter keys on your keyboard (A-Z or a-z); non lettered keys will not be put. Player will have 10 guesses throughout the game. Once the player rans out of guesses, its game over.
 
 Shout Outs:
+-T\
--- javascript/game.js
+++ javascript/game.js
@@ -1,85 +1,47 @@
 
-    // Just an alert to inform the user to press any Alpa keys to start the game
-    alert("Press any key to start!");
+// Just an alert to inform the user to press any Alpa keys to start the game
+alert("Press any key to start!");
 
-    // List of word for the computer to choose from
+// List of word for the computer to choose from
 
-    var wordBank = ["Aberdeen", "Anacortes", "Auburn", "Bellevue", "Bellingham", "Bremerton", "Centralia", "Coulee Dam", "Coupeville", "Ellensburg", "Ephrata", "Everett", "Hoquiam", "Kelso", "Kennewick", "Longview", "Moses Lake", "Oak Harbor", "Olympia", "Pasco", "Point Roberts", "Port Angeles", "Pullman", "Puyallup", "Redmond", "Renton", "Richland", "Seattle", "Spokane", "Tacoma", "Vancouver", "Walla Walla", "Wenatchee", "Yakima"
-    ];
+var wordBank = ["Aberdeen", "Anacortes", "Auburn", "Bellevue", "Bellingham", "Bremerton", "Centralia", "Coulee Dam", "Coupeville", "Ellensburg", "Ephrata", "Everett", "Hoquiam", "Kelso", "Kennewick", "Longview", "Moses Lake", "Oak Harbor", "Olympia", "Pasco", "Point Roberts", "Port Angeles", "Pullman", "Puyallup", "Redmond", "Renton", "Richland", "Seattle", "Spokane", "Tacoma", "Vancouver", "Walla Walla", "Wenatchee", "Yakima"
+];
 
-    // numver of max Guesses for the user
-    const maxGuess = 10;
+// numver of max Guesses for the user
+var maxGuess = 10;
+var letterGuessed = [];
+var guessingWord = [];
+var numGuess;
+var wins = 0;
+var currentWord;
+var isDone = false;
+var guessedLetterBox = document.getElementById("letterGuessed");
 
-    maxGuess--;
-
-    var letterGuessed = []
-    var guessingWord = []
-    var wordToMatch
-    var numGuess
-    var wins = 0 
-
-    document.onkeypress = function(event) {
-        if (Alpha(event.key) && !pauseGame) {
-            checkForLetter(event.key.toUpperCase))
-        }
-    }
-
-    for (var i=0, j = wordToMatch.length; i<j; i++) {
-        if (letter === wordToMatch[i]) {
-            guessingWord[i] = letter
-            found letter = true
-            
-            if (guessingWord.join("") === wordToMatch) {
-                wins++
-                pauseGame = true
-                updateDisplay(
-                    setTimeout(resetGame,5000)
-                )
-            }
-        }
-    } 
-
-    if (!letterGuessed.includes(letter)) {
-        letterGuessed.push(letter)
-        numGuess--
-    }
-    if (numGuess === 0) {
-        guessingWord = wordToMatch.split()
-        pauseGame = true
-        setTimeout(resetGame, 5000)
-    }
-
-    updateDisplay();
-
-    fuction Alpha(ch) {
-        return /^[A-Z]$i.test(ch);
-    }
-
-    function resetGame() {
-        numGuess = maxGuess
-        pauseGame =false
-    }
+document.onkeyup = function(event) {
+    userGuess = event.key;
+    // userGuess = [Math.floor(Math.random() *wordBank.length)];
+    guessedLetterBox.innerHTML+=userGuess + "," + " ";
+    console.log(guessedLetterBox)
+    console.log(userGuess)
     
-    wordToMatch = wordBank[Math.floor(Math.random() * wordBank.length)].toUpperCase(
-        console.log(wordToMatch)
+}
 
-        letterGuessed = []
-        guessingWord = []
-
-        for (var i=0, j=wordToMatch.length; i < j; i++) {
-            if (wordToMatch[i] === "") {
-                guessingWord.push("")
-            } else {
-                guessingWord.push("_")
-            }
-        }
-
-    )
-
-    function updateDisplay() {
-        document.getElementById("totalWins").innerText = wins
-        document.getElementById("currentWord").innerText = guessingWord.join("")
-        document.getElementById("remainingGuess").innerText = numGuess
-        document.getElementById("letterGuessed").innerText = letterGuessed.join("")
+function setup() {
+   
+    currentWord = words[Math.floor(Math.random() * wordBank.legnth)];
+    currentWordArr = [];
+    for (var i = 0; i < currentWord.length; i++) {
+        currentWordArr = [];
     }
+    numGuess = maxGuess;
+    letterGuessed = [];
+    console.log(wordBank);
+    updateScreen();
+};
+
+function updateDisplay() {
+    document.getElementById("totalWins").innerText = wins;
+    document.getElementById("currentWord").innerText = guessingWord.join("");
+    document.getElementById("remainingGuesses").innerText = numGuess;
+    document.getElementById("letterGuessed").innerText = letterGuessed.join("");
 }
